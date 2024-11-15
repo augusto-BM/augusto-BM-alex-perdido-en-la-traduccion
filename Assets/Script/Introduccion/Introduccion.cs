5cd13eea99d54d55d97924158e87f471cefe1180
Augusto BM <137666674+augusto-BM@users.noreply.github.com>
2024-11-14 23:32:17 -0500

    agregando introduccion y mejora de menu y escenario 1

---
+++ Assets/Script/Introduccion/Introduccion.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Introduccion : MonoBehaviour
+{
+     public VideoPlayer videoPlayer;
+    public string siguienteEscena = "PrimerEscenario";  
+
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();  
+        }
+
+        videoPlayer.loopPointReached += terminoVideo;
+    }
+
+    void terminoVideo(VideoPlayer vp)
+    {
+
+        SceneManager.LoadScene(siguienteEscena);
+    }
+}
